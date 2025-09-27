3e1e40f6aed4ee4e3f573e9901240a24fa657241
Stephen Kyle <stephenckyle@gmail.com>
2025-09-27 15:31:02 +0100

    supports playing the next track automatically again

--- lib/album_player.py
+++ lib/album_player.py
@@ -1,10 +1,49 @@
-import dotenv
 import os
 import subprocess
+import threading
+
+import dotenv
 
 import lib.api
 
 dotenv.load_dotenv()
+
+
+class ProcessWithCallback:
+    def __init__(self, command: list, callback):
+        self._terminate_event = threading.Event()
+        self._command = command
+        self._callback = callback
+        self._proc = None
+        self._thread = threading.Thread(target=self._run)
+        self._thread.start()
+
+    def _run(self):
+        self._proc = subprocess.Popen(
+            self._command,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        finished = False
+        while not finished:
+            try:
+                self._proc.wait(timeout=1)
+                finished = True
+            except subprocess.TimeoutExpired:
+                if self._terminate_event.is_set():
+                    self._proc.terminate()
+                    return
+
+        # I think this might be a problem because technically we're calling the
+        # callback WITHIN the thread spawned to run the process. Instead we need
+        # to post something back? TODO
+        # (but it seems to work...)
+
+        self._callback()
+
+    def terminate(self):
+        self._terminate_event.set()
+        self._thread.join()
 
 
 class AlbumPlayer:
@@ -13,7 +52,7 @@ class AlbumPlayer:
         self._album_data = album_data
         self._track_index = 0
         self._track_count = len(album_data["tracks"])
-        self._active_proc = None
+        self._proc = None
         self._api_key = os.environ["MIRLO_API_KEY"]
 
     def get_album_name(self) -> str:
@@ -22,15 +61,24 @@ class AlbumPlayer:
     def get_artist_name(self) -> str:
         return self._album_data["artist"]["name"]
 
+    def _track_finished_callback(self):
+        if self._track_index < self._track_count:
+            # The track finished, so the process/thread finished.
+            self._proc = None
+            self.play_next_track()
+        else:
+            self.stop()
+
     def play_next_track(self):
-        if self._active_proc and self._active_proc.poll() is None:
-            self._active_proc.terminate()
+        if self._proc:
+            self._proc.terminate()
+            self._proc = None
 
         track = self._album_data["tracks"][self._track_index]
         track_id = track["id"]
         self._track_index += 1
 
-        self._active_proc = subprocess.Popen(
+        self._proc = ProcessWithCallback(
             [
                 "ffplay",
                 "-nodisp",
@@ -40,19 +88,10 @@ class AlbumPlayer:
                 f"mirlo-api-key: {self._api_key}",
                 f"{lib.api.base_url}/tracks/{track_id}/stream/playlist.m3u8",
             ],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
+            self._track_finished_callback,
         )
 
     def stop(self):
-        if self._active_proc:
-            self._active_proc.terminate()
-            self._active_proc = None
-
-    def check_if_track_finished(self) -> bool:
-        if self._active_proc:
-            return self._active_proc.poll() is not None
-        return True
-
-    def is_finished(self) -> bool:
-        return self._track_index >= self._track_count
+        if self._proc:
+            self._proc.terminate()
+            self._proc = None
